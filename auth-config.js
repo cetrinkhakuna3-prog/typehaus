26df3647e024b388cd02081e6837e9165d6960ce
cetrinkhakuna3-prog <cetrinkhakuna3@gmail.com>
2026-06-10 00:24:36 +0700

    Update live Typehaus site

--- auth-config.js
+++ auth-config.js
@@ -1,5 +1,15 @@
+const TYPEHAUS_PUBLIC_SITE_URL = "https://cetrinkhakuna3-prog.github.io/typehaus";
+const currentPageName = window.location.pathname.split("/").pop() || "index.html";
+const hostedRecoveryUrl = `${TYPEHAUS_PUBLIC_SITE_URL}/${currentPageName}`;
+const isLocalPreview =
+  window.location.protocol === "file:" ||
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === "localhost";
+
 window.AUTH_CONFIG = {
   supabaseUrl: "https://piivygeapqvhjuppxhuy.supabase.co",
   supabaseAnonKey: "sb_publishable_Mt7Of9hnsKpY8zczoTQtyg_zgGwqCyP",
-  redirectTo: window.location.origin + window.location.pathname
+  publicSiteUrl: TYPEHAUS_PUBLIC_SITE_URL,
+  redirectTo: isLocalPreview ? hostedRecoveryUrl : window.location.origin + window.location.pathname,
+  adminEmails: "cetrinkhakuna3@gmail.com"
 };
