926a750790182746b588ffb5163e79222b4349d4
cetrinkhakuna3-prog <cetrinkhakuna3@gmail.com>
2026-06-09 21:13:40 +0700

    Add files via upload
    Update payment return and download flow

---
+++ auth-config.js
@@ -0,0 +1,5 @@
+window.AUTH_CONFIG = {
+  supabaseUrl: "https://piivygeapqvhjuppxhuy.supabase.co",
+  supabaseAnonKey: "sb_publishable_Mt7Of9hnsKpY8zczoTQtyg_zgGwqCyP",
+  redirectTo: window.location.origin + window.location.pathname
+};
